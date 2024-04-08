9e1d367576eb70562082a6b8c82e5105e103ea5e
Tameno-01 <tamenorias@gmail.com>
2024-04-08 10:38:25 +0200

    Improved camera shake modifier

--- addons/modular_camera/modifiers/shake.gd
+++ addons/modular_camera/modifiers/shake.gd
@@ -6,30 +6,23 @@ extends CameraModifier
 
 const FLIP_OFFSET = TAU / 2
 
-
+## Press this to reset the shake in the inspector.
 @export var _reset := false: # _ Because users aren't meant to access this value through code
 		set = _set_reset
 ## How intentse the shake is
 @export var intensity: float = 0.1
 ## How fast the shake is
 @export var speed: float = 40.0
-## How fast the shake gets less instense.
+## How fast the shake gets less instense, the duration of the shake is 1 / decay.
 @export var decay: float = 0.0
-## Wether to remove this modifier when the shake has lost all of it's intensity, only relevnt if decay is not 0.
-@export var auto_remove: bool = true
 ## How much tha camera should shake horizontally.
 @export var horizontal_intensity: float = 1.0
 ## How much the camera should shake veritically.
 @export var vertical_intenisty: float = 1.0
-## How fast the horizontal shake should be
-@export var horizontal_speed: float = 1.0
-## How much to randomize the speed of the horizontal shake.
-@export var horizontal_speed_random: float = 0.5
-## How fast the vertical shake should be
-@export var vertical_speed: float = 1.0
-## How much to randomize the speed of the vertical shake.
-@export var vertical_speed_random: float = 0.5
-
+## How much to randomize the speed of the horizontal and vertial shakes.
+@export_range(0.0, 0.99) var speed_random: float = 0.3
+## Wether to remove this modifier when the shake has lost all of it's intensity, only relevnt if decay is not 0.
+@export var auto_remove: bool = true
 
 var _horizontal_progress: float
 var _vertical_progress: float
@@ -43,8 +36,14 @@ func _on_start():
 
 
 func _process(delta: float):
-	_horizontal_progress += speed * _actual_horizontal_speed * delta
-	_vertical_progress += speed * _actual_vertical_speed * delta
+	_horizontal_progress +=_actual_horizontal_speed * delta
+	_vertical_progress += _actual_vertical_speed * delta
+	if _horizontal_progress > TAU:
+		_horizontal_progress -= TAU
+		_actual_horizontal_speed = speed * (1.0 - randf() * speed_random)
+	if _vertical_progress > TAU:
+		_vertical_progress -= TAU
+		_actual_vertical_speed = speed * (1.0 - randf() * speed_random)
 	_decay_left -= decay * delta
 	if _decay_left <= 0.0:
 		if auto_remove and not Engine.is_editor_hint():
@@ -62,8 +61,8 @@ func reset():
 	_horizontal_progress = 0 if randf() < 0.5 else FLIP_OFFSET
 	_vertical_progress = 0 if randf() < 0.5 else FLIP_OFFSET
 	_decay_left = 1.0
-	_actual_horizontal_speed = horizontal_speed * (1.0 - randf() * horizontal_speed_random)
-	_actual_vertical_speed = vertical_speed * (1.0 - randf() * vertical_speed_random)
+	_actual_horizontal_speed = speed * (1.0 - randf() * speed_random)
+	_actual_vertical_speed = speed * (1.0 - randf() * speed_random)
 
 
 func _set_reset(value: bool):
