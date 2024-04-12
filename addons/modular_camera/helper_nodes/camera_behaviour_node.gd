53faccb053d8a23883aa6f6d3a3f1581c0ea2686
Tameno-01 <tamenorias@gmail.com>
2024-04-12 20:05:31 +0200

    Added reference frame setting to CameraBehaviourNode

--- addons/modular_camera/helper_nodes/camera_behaviour_node.gd
+++ addons/modular_camera/helper_nodes/camera_behaviour_node.gd
@@ -4,7 +4,7 @@ class_name CameraBehaviourNode
 extends Node3D
 
 
-enum target_update_modes {
+enum update_modes {
 	NEVER,
 	ON_READY,
 	PROCESS,
@@ -19,11 +19,13 @@ enum target_update_modes {
 @export var _preview_behaviour: bool = false:
 		set = _set_preview_behaviour
 ## When to update the behaviour's target_override to this node's position, only relevant if the behaviour's override_target is true.
-@export var target_update_mode: target_update_modes = target_update_modes.NEVER:
+@export var target_update_mode: update_modes = update_modes.NEVER:
 		set = _set_target_update_mode
 ## Press this button to instantly update the behaviour's target_override to this node's position, only relevant if the behaviour's override_target is true.
 @export var _update_target: bool = false:
 		set = _set_update_target
+## Wether to update the behaviours reference frame along with the target.
+@export var also_update_reference_frame: bool = false
 
 ## Adds the behaviour to the camera.
 func add_behaviour():
@@ -37,6 +39,8 @@ func remove_behaviour():
 func update_target():
 	if behaviour:
 		behaviour.target_override = global_position
+		if also_update_reference_frame:
+			behaviour.reference_frame_override = global_transform.basis
 
 ## Makes the current camera (needs to be set) preview the behaviour.
 func preview_behaviour():
@@ -46,7 +50,7 @@ func preview_behaviour():
 
 func _ready():
 	_update_target_update_mode()
-	if target_update_mode == target_update_modes.ON_READY:
+	if target_update_mode == update_modes.ON_READY:
 		update_target()
 
 
@@ -59,29 +63,27 @@ func _physics_process(_delta: float):
 
 
 func _actual_process(_delta: float):
-	if behaviour:
-		behaviour.target_override = global_position
-
-
-func _set_target_update_mode(value: target_update_modes):
-	target_update_mode = value
-	_update_target_update_mode()
+	update_target()
 
 
 func _update_target_update_mode():
 	match target_update_mode:
-		target_update_modes.NEVER:
+		update_modes.NEVER:
 			set_process(false)
 			set_physics_process(false)
-		target_update_modes.ON_READY:
+		update_modes.ON_READY:
 			set_process(false)
 			set_physics_process(false)
-		target_update_modes.PROCESS:
+		update_modes.PROCESS:
 			set_process(true)
 			set_physics_process(false)
-		target_update_modes.PHYSICS_PROCESS:
+		update_modes.PHYSICS_PROCESS:
 			set_process(false)
 			set_physics_process(true)
+
+func _set_target_update_mode(value: update_modes):
+	target_update_mode = value
+	_update_target_update_mode()
 
 
 func _set_update_target(value: bool):
